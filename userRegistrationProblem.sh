5bbb4dda18e458e05c8f8f8cf2cbdfd3fa086abe
unknown <miyashaheen1995@gmail.com>
2020-11-07 00:45:22 +0530

    Added:functionalities

--- userRegistrationProblem.sh
+++ userRegistrationProblem.sh
@@ -1,6 +1,7 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 echo -e "\n**********User Registration Validation Problem! ***********\n"
+#check first name (Starting with capital and has min 3 characters)
 read -p "Enter First Name : " user_name
 username_pattern="^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
 if [[ $user_name =~ $username_pattern ]]
@@ -9,3 +10,14 @@ then
 else
 	echo "Invalid First name!"
 fi
+
+#check last name (Starting with capital and has min 3 characters)
+echo -e "\n"
+read -p "Enter Last Name : " last_name
+if [[ $last_name =~ $username_pattern ]]
+then
+        echo "Valid Last name!"
+else
+        echo "Invalid Last name!"
+fi
+
